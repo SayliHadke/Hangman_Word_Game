b841fed15fa2ade5e06fe64bbb1ec13d76a43d9e
SayliHadke <saylihadke21@gmail.com>
2024-12-10 21:25:31 +0530

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HANGMAN
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HANGMAN
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
